3dfbbe8eadc91c213d96ecadb01458086029bd98
Coco-hw <81812322+Coco-hw@users.noreply.github.com>
2023-06-10 23:14:28 +0900

    video error solved

--- src/components/Bundle.js
+++ src/components/Bundle.js
@@ -140,6 +140,7 @@ const Bundle = ({
 
     // get video & canvas
     const video = videoRef.current;
+    if(!video){return;}
     video.srcObject = stream;
     const canvas = document.getElementById("canvas");
 
